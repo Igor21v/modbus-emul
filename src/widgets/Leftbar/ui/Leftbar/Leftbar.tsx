407a9a3c8f5697ad018ff15ead4aa718e1012b10
Igor21v <igor21v@mail.ru>
2024-09-09 16:59:38 +0800

    build: add nav battons

--- src/widgets/Leftbar/ui/Leftbar/Leftbar.tsx
+++ src/widgets/Leftbar/ui/Leftbar/Leftbar.tsx
@@ -1,6 +1,8 @@
 import { memo } from 'react';
 import cls from './Leftbar.module.css';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button';
+import { VStack } from 'shared/ui/Stack';
 
 interface LeftbarProps {
   className?: string;
@@ -10,6 +12,10 @@ export const Leftbar = memo((props: LeftbarProps) => {
   const { className } = props;
 
   return (
-    <div className={classNames(cls.Leftbar, {}, [className])}>Навигация</div>
+    <VStack gap="16" justify="center" align="center" className={cls.Leftbar}>
+      <Button>Режим Мастер</Button>
+      <Button>Режим Слейв</Button>
+      <Button>Режим прослушки порта</Button>
+    </VStack>
   );
 });
