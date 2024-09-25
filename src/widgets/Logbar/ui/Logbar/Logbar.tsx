994703540e36757c5904f592cc14e6b79faf3f07
Igor21v <igor21v@mail.ru>
2024-09-25 17:02:16 +0800

    build: adding clear icon

--- src/widgets/Logbar/ui/Logbar/Logbar.tsx
+++ src/widgets/Logbar/ui/Logbar/Logbar.tsx
@@ -1,6 +1,8 @@
 import { memo } from 'react';
 import { HStack } from 'shared/ui/Stack';
 import { LogbarExpandIcon } from './LogbarExpandIcon';
+import Clear from 'shared/icons/Clear';
+import { Icon } from 'shared/ui/Icon';
 
 interface LogbarProps {
   className?: string;
@@ -12,6 +14,7 @@ export const Logbar = memo((props: LogbarProps) => {
   return (
     <HStack justify="between">
       Панель настройки логов
+      <Icon Svg={Clear} />
       <LogbarExpandIcon />
     </HStack>
   );
