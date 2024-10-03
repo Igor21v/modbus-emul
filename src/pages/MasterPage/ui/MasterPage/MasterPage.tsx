4bd30c48ce2411e89c5d29dbd969c664caf7cfb9
Igor21v <igor21v@mail.ru>
2024-10-03 16:40:51 +0800

    build: install jsmodbus

--- src/pages/MasterPage/ui/MasterPage/MasterPage.tsx
+++ src/pages/MasterPage/ui/MasterPage/MasterPage.tsx
@@ -9,6 +9,27 @@ interface MasterPageProps {
 export const MasterPage = memo((props: MasterPageProps) => {
   const { className } = props;
 
+  const Modbus = require('jsmodbus');
+  const net = require('net');
+  /*   const socket = new net.Socket();
+  const client = new Modbus.client.TCP(socket, 1);
+  const options = {
+    host: '127.0.0.1',
+    port: 502,
+  };
+  // use socket.on('open', ...) when using serialport
+  socket.on('connect', function () {
+    // make some calls
+
+    client.readCoils(0, 13).then(function (resp: any) {
+      // resp will look like { response : [TCP|RTU]Response, request: [TCP|RTU]Request }
+      // the data will be located in resp.response.body.coils: <Array>, resp.response.body.payload: <Buffer>
+
+      console.log(resp);
+    }, console.error);
+  });
+
+  socket.connect(options); */
   return (
     <div className={classNames(cls.MasterPage, {}, [className])}>
       Страница режима Мастер
