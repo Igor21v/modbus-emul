acf7bb19c9d2984c18fc6b425f98adbf4754ebd5
Igor Bondarenko <igor21v@mail.ru>
2024-12-08 21:52:49 +0800

    build: add wrap and unwrap icons

--- src/features/MasterRequests/ui/MasterRequests/MasterRequests.tsx
+++ src/features/MasterRequests/ui/MasterRequests/MasterRequests.tsx
@@ -1,6 +1,11 @@
-import { memo } from 'react';
-import { Settings } from '../Settings/Settings';
+import { memo, useState } from 'react';
+import Unwrap from 'shared/icons/Unwrap';
+import Wrap from 'shared/icons/Wrap';
+import { Icon } from 'shared/ui/Icon';
+import { VStack } from 'shared/ui/Stack';
 import { Results } from '../Results/Results';
+import { Settings } from '../Settings/Settings';
+import cls from './MasterRequests.module.css';
 
 interface MasterRequestsProps {
   className?: string;
@@ -8,9 +13,14 @@ interface MasterRequestsProps {
 
 export const MasterRequests = memo((props: MasterRequestsProps) => {
   const { className } = props;
+  const [showConfig, setShowConfig] = useState(true);
+
   return (
-    <>
-      <Settings /> <Results />
-    </>
+    <VStack className={cls.MasterRequests} gap="16" align="stretch">
+      <Icon Svg={Unwrap} hint="Развернуть конфигурацию запросов" onClick={() => setShowConfig(true)} height={50} width={50} />
+      <Icon Svg={Wrap} hint="Свернуть конфигурацию запросов" onClick={() => setShowConfig(false)} height={50} width={50} />
+      <Settings />
+      <Results />
+    </VStack>
   );
 });
---
+++ src/shared/icons/Unwrap.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from 'react';
+const Unwrap = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 310 310" {...props}>
+    <path d="m302.445 80.796-11.101-11.103c-6.123-6.131-16.074-6.131-22.209 0L153.67 183.707 37.907 67.959c-6.134-6.13-16.08-6.13-22.209 0L4.597 79.06c-6.129 6.133-6.129 16.067 0 22.201l137.83 137.829c6.129 6.136 16.067 6.136 22.203 0l137.815-136.096c6.144-6.13 6.144-16.068 0-22.198z" />
+  </svg>
+);
+export default Unwrap;
