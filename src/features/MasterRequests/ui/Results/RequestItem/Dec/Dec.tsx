79bcf84dc86c0f26fe2add70524f3d87ee6ba831
Igor Bondarenko <igor21v@mail.ru>
2025-01-20 22:22:45 +0800

    build: improve hex input

--- src/features/MasterRequests/ui/Results/RequestItem/Dec/Dec.tsx
+++ src/features/MasterRequests/ui/Results/RequestItem/Dec/Dec.tsx
@@ -1,18 +1,15 @@
 import { memo } from 'react';
-import cls from './Dec.module.css';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { TextSpan } from 'shared/ui/TextSpan';
 import { InputNum } from 'shared/ui/InputNum';
+import cls from './Dec.module.css';
 
 interface DecProps {
-  className?: string;
   value: number;
   editable: boolean;
   setContentReg: (content: number) => void;
 }
 
 export const Dec = memo((props: DecProps) => {
-  const { className, value, editable, setContentReg } = props;
+  const { value, editable, setContentReg } = props;
   const renderVal = value.toString(10);
 
   return (
--- src/features/MasterRequests/ui/Results/RequestItem/Hex/Hex.tsx
+++ src/features/MasterRequests/ui/Results/RequestItem/Hex/Hex.tsx
@@ -3,19 +3,24 @@ import { Input } from 'shared/ui/Input';
 import cls from './Hex.module.css';
 
 interface HexProps {
-  className?: string;
   value: number;
   setContentReg: (content: number) => void;
   editable: boolean;
 }
 
 export const Hex = memo((props: HexProps) => {
-  const { className, value, setContentReg, editable } = props;
+  const { value, setContentReg, editable } = props;
   const renderVal = value.toString(16);
   const setContent = useCallback(
-    (content: string) => {
-      const valNum = parseInt(content, 16);
+    (val: string) => {
+      let valNum = parseInt(val, 16);
+      if (Number.isNaN(valNum) || valNum < 0) {
+        valNum = 0;
+      } else if (valNum > 65535) {
+        valNum = 65535;
+      }
       setContentReg(valNum);
+      console.log(valNum);
     },
     [renderVal],
   );
